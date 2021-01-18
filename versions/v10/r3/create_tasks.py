f0a497aec3f3a817ac76f3904771298390e111b7
jodafons <jodafons@lps.ufrj.br>
2021-01-18 18:14:01 -0300

    update

--- versions/v10/r3/create_tasks.py
+++ versions/v10/r3/create_tasks.py
@@ -14,8 +14,11 @@ command = """maestro.py task create \
   --exec "run_tuning.py -c %IN -d %DATA -r %REF -v %OUT -t v10 -b zee -p r3" \
   --queue "gpu" """
 
+try:
+    os.makedirs(path)
+except:
+    pass
 
-os.makedirs(path)
 for et in range(5):
     for eta in range(5):
         ref = "{'%%REF':'user.jodafons.data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%d_eta%d.ref.pic.gz'}"%(et,eta)
