a53e08b5478b35dc3d94b1e131c2e322c5a4e243
Juan Lieber Marin <juan.lieber.marin@gmail.com>
2021-02-03 20:52:34 -0300

    fix small bugs

--- versions/v2/create_jobs.py
+++ versions/v2/create_jobs.py
@@ -1,14 +1,10 @@
 from saphyra import *
 # ppChain
-from saphyra import PreProcChain_v1, Norm1, ReshapeToConv1D
-pp = PreProcChain_v1( [Norm1()] )
-
-# tensorflow
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Conv1D, Flatten
 
 # function to define the keras model
 def get_model(neuron_min, neuron_max):
+  from tensorflow.keras.models import Sequential
+  from tensorflow.keras.layers import Dense, Dropout, Activation, Conv1D, Flatten
   modelCol = []
   for n in range(neuron_min,neuron_max+1):
     model = Sequential()
@@ -19,17 +15,12 @@ def get_model(neuron_min, neuron_max):
   return modelCol
 
 # cross-validation method
-n_folds = 10 # normally 10 folds
-from sklearn.model_selection import StratifiedKFold, KFold
-kf = StratifiedKFold(n_splits=n_folds, random_state=512, shuffle=True)
 
 n_max_neuron = 10
 n_min_neuron = 2
-createPandaJobs( 
+create_jobs( 
         models       = get_model(neuron_min=n_min_neuron,
                                  neuron_max=n_max_neuron),
-        ppChain       = pp,
-        crossVal      = kf,
         nInits        = 10,
         nInitsPerJob  = 1,
         sortBounds    = 10,
