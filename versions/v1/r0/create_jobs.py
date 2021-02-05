101bb3d58cc762544161cbec7f13c2061a168c10
jodafons <jodafons@lps.ufrj.br>
2021-02-05 20:33:18 -0300

    fix some scritps

--- versions/v1/r0/create_jobs.py
+++ versions/v1/r0/create_jobs.py
@@ -1,7 +1,4 @@
 from saphyra import *
-# ppChain
-from saphyra import PreProcChain_v1, Norm1, ReshapeToConv1D
-pp = PreProcChain_v1( [Norm1()] )
 
 # tensorflow
 from tensorflow.keras.models import Sequential
@@ -12,29 +9,26 @@ def get_model(neuron_min, neuron_max):
   modelCol = []
   for n in range(neuron_min,neuron_max+1):
     model = Sequential()
-    model.add(Dense(n, input_shape=(100,), activation='tanh'))
-    model.add(Dense(1, activation='linear'))
-    model.add(Activation('tanh'))
+    model.add(Dense(n, input_shape=(100,), activation='tanh', name='dense_layer'))
+    model.add(Dense(1, activation='linear', name='output_for_inference'))
+    model.add(Activation('tanh', name='output_for_training'))
     modelCol.append(model)
   return modelCol
 
 # cross-validation method
 n_folds = 10 # normally 10 folds
-from sklearn.model_selection import StratifiedKFold, KFold
-kf = StratifiedKFold(n_splits=n_folds, random_state=512, shuffle=True)
 
-n_max_neuron = 10
 n_min_neuron = 2
-n_inits      = 5
-createPandaJobs( 
+n_max_neuron = 10
+n_inits      = 10
+
+create_jobs( 
         models       = get_model(neuron_min=n_min_neuron,
                                  neuron_max=n_max_neuron),
-        ppChain       = pp,
-        crossVal      = kf,
         nInits        = 5,
         nInitsPerJob  = 1,
         sortBounds    = 10,
         nSortsPerJob  = 1,
         nModelsPerJob = 5,
-        outputFolder  = 'job_config.Zrad_v1.n2to10.10sorts.5inits',
+        outputFolder  = 'job_config.Zrad_v1.n2to10.10sorts.10inits.r0',
         )
