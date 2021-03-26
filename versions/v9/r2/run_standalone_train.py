76c67d920371db1791ad96c49182c1271467f3b0
Edmar Egidio <edmar.egidio@caloba21.lps.ufrj.br>
2021-03-26 19:15:35 -0300

    Update runStandalone

--- versions/v9/r2/run_standalone_train.py
+++ versions/v9/r2/run_standalone_train.py
@@ -4,17 +4,22 @@ import glob
 from itertools import product
 
 # define the paths into the container
-data_path    = '/home/Derivations/NPZ/NPZ1/dataset17_13TeV/data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%i_eta%i.npz'
-ref_path     = '/home/RingerDev/Tuning/RingerRp/r0/ref/data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%i_eta%i.ref.pic.gz'
-#config_path  = '/home/RingerDev/Tuning/RingerRp/r0/config/job_config.Zee_Rp.n2_10.10sorts.10inits.r0/*'
-config_path = '/home/RingerDev/Tuning/RingerRp/r0/config/job_config.Zee_Rp.n5.10sorts.5inits.r0/*'
-output_path  = '/home/RingerDev/Tuning/RingerRp/r0/output/user.eegidiop.data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%i_eta%i.v9.r2'
+#data_path    = '/home/Derivations/NPZ/NPZ1/dataset17_13TeV/data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%i_eta%i.npz'
+#ref_path     = '/home/RingerDev/Tuning/RingerRp/r0/ref/data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%i_eta%i.ref.pic.gz'
+#config_path = '/home/RingerDev/Tuning/RingerRp/r0/config/job_config.Zee_Rp.n5.10sorts.5inits.r0/*'
+#output_path  = '/home/RingerDev/Tuning/RingerRp/r0/output/user.eegidiop.data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%i_eta%i.v9.r2'
+
+data_path = '/home/edmar.egidio/datasets/npz/NPZ/dataset17_13TeV/data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%i_eta%i.npz'
+ref_path = '/home/edmar.egidio/Tunings/RingerRp/r0/ref/data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%i_eta%i.ref.pic.gz'
+config_path = '/home/edmar.egidio/Tunings/RingerRp/r0/config/job_config.Zee_Rp.n5.10sorts.5inits.r0/*'
+output_path = '/home/edmar.egidio/Tunings/RingerRp/r0/output/user.eegidiop.data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%i_eta%i.rp.r0'
+
 # create a list of config files
 config_list  = glob.glob(config_path)
 print(config_list)
 
 # loop over the bins
-for iet, ieta in product(range(3,5), range(5)):
+for iet, ieta in product(range(5), range(5)):
     print('Processing -> et: %i | eta: %i' %(iet, ieta))
     # format the names
     data_file = data_path %(iet, ieta)
