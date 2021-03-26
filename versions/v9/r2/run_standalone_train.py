6210d9cb8ce12cfba4c68b860318741e17db1a89
edmaregidio <edmaregidio@gmail.com>
2021-03-26 19:25:33 -0300

    Update run_standalone_train.py

--- versions/v9/r2/run_standalone_train.py
+++ versions/v9/r2/run_standalone_train.py
@@ -11,7 +11,7 @@ from itertools import product
 
 data_path = '/home/edmar.egidio/datasets/npz/NPZ/dataset17_13TeV/data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%i_eta%i.npz'
 ref_path = '/home/edmar.egidio/Tunings/RingerRp/r0/ref/data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%i_eta%i.ref.pic.gz'
-config_path = '/home/edmar.egidio/Tunings/RingerRp/r0/config/job_config.Zee_Rp.n5.10sorts.5inits.r0/*'
+config_path = '/home/edmar.egidio/Tunings/RingerRp/r0/config/job_config.Zee_Rp.2n10.10sorts.10inits.r0/*'
 output_path = '/home/edmar.egidio/Tunings/RingerRp/r0/output/user.eegidiop.data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et%i_eta%i.rp.r0'
 
 # create a list of config files
