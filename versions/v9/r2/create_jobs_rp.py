4d684b921441ccf5245bacd78ff8358cd0172c09
edmaregidio <edmaregidio@gmail.com>
2021-03-26 19:24:13 -0300

    Update create_jobs_rp.py

--- versions/v9/r2/create_jobs_rp.py
+++ versions/v9/r2/create_jobs_rp.py
@@ -6,20 +6,24 @@ from tensorflow.keras.layers import Layer
 import tensorflow as tf
 from tensorflow.keras import layers
 
-inputs = layers.Input(shape=(100,), name='Input_rings')
-input_rp = RpLayer()(inputs)
-dense_rp = layers.Dense(5, activation='tanh', name='dense_rp_layer')(input_rp)
-dense = layers.Dense(1,activation='linear', name='output_for_inference')(dense_rp)
-outputs = layers.Activation('tanh', name='output_for_training')(dense)
-model = tf.keras.Model(inputs, outputs, name = "model")
+def get_model( ):
+        modelCol = []
+        for n in range(2,10+1):
+                inputs = layers.Input(shape=(100,), name='Input_rings')
+                input_rp = RpLayer()(inputs)
+                dense_rp = layers.Dense(5, activation='tanh', name='dense_rp_layer')(input_rp)
+                dense = layers.Dense(1,activation='linear', name='output_for_inference')(dense_rp)
+                outputs = layers.Activation('tanh', name='output_for_training')(dense)
+                model = tf.keras.Model(inputs, outputs, name = "model")
+                modelCol.append(model)
 
 
-create_jobs( models = [model],
-        nInits        = 5,
+create_jobs( models = get_model(),
+        nInits        = 10,
         nInitsPerJob  = 1,
         sortBounds    = 10,
         nSortsPerJob  = 1,
         nModelsPerJob = 1,
-        outputFolder  = 'job_config.Zee_Rp.n5.10sorts.5inits.r0' )
+        outputFolder  = 'job_config.Zee_Rp.2n10.10sorts.10inits.r0' )
 
 
