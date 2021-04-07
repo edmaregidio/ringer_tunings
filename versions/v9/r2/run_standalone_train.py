508fa9886250d436804bed82b0f5e45eef931712
edmaregidio <edmaregidio@gmail.com>
2021-04-07 14:39:22 -0300

    Update run_standalone_train.py

--- versions/v9/r2/run_standalone_train.py
+++ versions/v9/r2/run_standalone_train.py
@@ -19,7 +19,7 @@ config_list  = glob.glob(config_path)
 print(config_list)
 
 # loop over the bins
-for iet, ieta in product(range(5), range(5)):
+for iet, ieta in product(range(1,5), range(5)):
     print('Processing -> et: %i | eta: %i' %(iet, ieta))
     # format the names
     data_file = data_path %(iet, ieta)
